495156f27cb68ce10f3a974c8a76e5e4152418dd
MarkKra88 <maciej.krawczak88@gmail.com>
2025-04-25 13:37:37 +0100

    Add build_url method

--- src/core/base_rest_client.py
+++ src/core/base_rest_client.py
@@ -1,6 +1,11 @@
-from typing import Dict, Optional
+from typing import Dict, Optional, Any
+from urllib.parse import urlencode
 
-class BaseRESTclient:
+class BaseRESTClient:
+
+    """
+    Initialize the REST client with a base URL, optional headers, and optional authentication token.
+    """
     def __init__(self,
                  base_url: str,
                  headers: Optional[Dict[str, str]] = None,
@@ -10,3 +15,14 @@ class BaseRESTclient:
         self.headers = headers or {}
         if auth_token:
             self.headers["Authorization"] = f"{auth_type} {auth_token}"
+
+    """
+        Build the full request URL by combining the base URL, endpoint, and any query parameter.
+    """
+    def build_url(self,
+                  endpoint:str = "",
+                  params: Optional[Dict[str, Any]] = None) ->str:
+        url = f"{self.base_url.rstrip(f'/')}/{endpoint.lstrip('/')}"
+        if params:
+            query_string = urlencode(params)
+            url = f"{url}?{query_string}"
