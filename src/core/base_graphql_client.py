4eafef1576acf31be80a5a66d7b0dc5210805259
MarkKra88 <maciej.krawczak88@gmail.com>
2025-05-05 20:56:44 +0100

    Create 'query' function

--- src/core/base_graphql_client.py
+++ src/core/base_graphql_client.py
@@ -1,4 +1,6 @@
-from typing import Optional, Dict
+from typing import Optional, Dict, Any
+from src.core.utils import log_errors
+import requests
 
 class BaseGraphQLClient:
     """
@@ -15,3 +17,25 @@ class BaseGraphQLClient:
         self.headers = headers or {}
         if "Content-Type" not in self.headers:
             self.headers["Content-Type"] = "application/json"
+
+    @log_errors
+    def query(self,
+              query: str,
+              variables: Optional[Dict[str, Any]]) -> Dict[str, Any]:
+        """
+        Execute a GraphQL query with optional variables.
+        Returns the JSON response as a dictionary.
+        """
+        payload =  {
+            "query": query,
+            "variables": variables or {}
+        }
+
+        response = requests.post(
+            self.endpoint,
+            headers = self.endpoint,
+            json = payload,
+            timeout = 10
+        )
+        response.raise_for_status()
+        return response.json()
